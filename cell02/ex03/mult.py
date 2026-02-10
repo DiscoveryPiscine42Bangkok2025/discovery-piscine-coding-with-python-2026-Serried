1f281a51c757ef982673324b7e88e2b7d55d200f
Serried <contact.rewmb@gmail.com>
2026-02-10 22:29:56 +0700

    add cell2-3

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,9 @@
+first = int(input("Enter the first number: "))
+second = int(input("Enter the second number: "))
+
+if first * second < 0:
+    print("The result is negative.")
+elif first * second > 0:
+    print("The result is positive.")
+else:
+    print("The result is positive and negative")
